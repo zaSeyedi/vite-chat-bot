c45acc131858fa2d52f258185abd5b334b504d59
zahra_sey <za.seyyedi@gmail.com>
2023-12-31 18:53:00 -0800

    back to Link redirect

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import ContainerLayout from '../../layout/containerLayout';
-import axios from "axios";
+import { Link } from "react-router-dom";
+// import { useEffect, useState } from 'react';
+// import axios from "axios";
 
 interface IProps {
 }
 const LoginPage: React.FC<IProps> = () => {
 
-    
-    const handleSSOLogin = async () => {
-        try {
-          const response = await axios.get('http://78.157.46.108:8080/user/ssoLogin');
-          window.location.href = response.data.redirectUrl;
-          console.log(response)
-        } catch (error) {
-          console.error('Error during SSO login:', error);
-        }
-      };
+    // const [url, setUrl] = useState('');
+
+
+    // const handleSSOLogin = async () => {
+    //     try {
+    //       const response = await axios.get('http://78.157.46.108:8080/ssoLogin');
+    //       window.location.href = response.data.redirectUrl;
+    //     } catch (error) {
+    //       console.error('Error during SSO login:', error);
+    //     }
+    //   };
 
     return (
         <ContainerLayout>
-            <button
+          <Link
                 className='cursor-pointer font-bold text-md bg-slate-300 flex w-1/2 mt-20 mx-auto p-3 rounded-md justify-center items-center'
-                onClick={handleSSOLogin}>
+                to={`http://78.157.46.108:8080/user/ssoLogin`}
+                onClick={() => console.log('link click')}>
                 ورود به درگاه یکپارچه دناپ
-            </button>
+            </Link>
         </ContainerLayout>
     );
 }
