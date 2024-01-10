62844c442f1bbedb9036cbbe84d5252d9eb0f127
zahra_sey <za.seyyedi@gmail.com>
2024-01-09 20:37:53 -0800

    es lint

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContainerLayout from '../../layout/containerLayout';
-import { Link, redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import axios from "axios";
 
